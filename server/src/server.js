a5eb5a45156d9562b5cd4286a719e17b281c4fed
alejandroimen <aimr2910@gmail.com>
2024-07-19 08:41:19 -0600

    Correccion de estilos

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const usersRoutes = require('./routes/users');
 const usersJWTRoutes = require('./routes/users_jwt');
 const ventasRoutes = require('./routes/ventas');
 const proveedoresRoutes = require('./routes/proveedores');
@@ -27,7 +26,6 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-app.use('/users', usersRoutes);
 app.use('/usersJWT', usersJWTRoutes);
 app.use('/ventas', ventasRoutes);
 app.use('/proveedores', proveedoresRoutes);
@@ -40,9 +38,5 @@ app.use('/products',productosMLRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor Express en ejecución en http://localhost:${port}`);
-  /* // Refresca el token cada 5 horas y 50 minutos (350 minutos)
-  setInterval(refreshAccessToken, 350 * 60 * 1000);
-  // Llama a la función al iniciar la aplicación
-  refreshAccessToken(); */ //Esto ya lo tengo en otro lado (al momento de logiarse)
 });
 
